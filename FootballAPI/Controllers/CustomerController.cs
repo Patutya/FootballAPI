1ec7604bcf7a1942c809c72e51f96778cef40303
saba liluashvili <sabaliluashvili12@gmail.com>
2022-11-19 12:20:15 +0400

    Add project files.

---
+++ FootballAPI/Controllers/CustomerController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FootballAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CustomerController : ControllerBase
+    {
+        [HttpGet(Name = "GetAllCustomers")]
+
+        public IEnumerable<Models.Person> GetAllCustomers([FromQuery] Models.Person person)
+        {
+            return Enumerable.Range(1, 5).Select(index => new Models.Person
+            {
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ FootballAPI/Models/Person.cs
@@ -0,0 +1,10 @@
+﻿namespace FootballAPI.Models
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Surname { get; internal set; }
+        public int Money { get; internal set; }
+    }
+}
